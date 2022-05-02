12453120066d1054e122fcc1970143ff370e1fa4
Alyssa <alyssanm18@gmail.com>
2022-05-01 23:03:46 -0600

    fijado

--- makeDB.py
+++ makeDB.py
@@ -2,6 +2,7 @@ import os
 
 import pandas as pd
 
+
 class Data:
 
     def __init__(self, input_file):
@@ -18,33 +19,33 @@ class Data:
         self.base_salary = df['Base Salary']
 
     def people_record(self):
-        people_stack = pd.concat([self.Lastname, self.Firstname, self.status, self.salary, self.hours, self.title], axis=1)
+        people_stack = pd.concat(
+            [self.Lastname, self.Firstname, self.status, self.salary, self.hours, self.title], axis=1)
         return people_stack
 
     def agency_record(self):
-        agency_stack = pd.concat([agency, location], axis=1)
+        agency_stack = pd.concat([self.agency, self.location], axis=1)
         return agency_stack
 
-
     def jobs_record(self):
-        jobs_stack = pd.concat([title, base_salary], axis=1)
-        return agency_stack 
-    
+        jobs_stack = pd.concat([self.title, self.base_salary], axis=1)
+        return jobs_stack
+
     def getLastname(self):
         return self.Lastname
 
     def getFirstname(self):
         return self.Firstname
-    
+
     def getStatus(self):
         return self.status
-    
+
     def getSalary(self):
         return self.salary
-    
+
     def getHours(self):
         return self.hours
-    
+
     def getJobTitle(self):
         return self.title
 
@@ -58,17 +59,17 @@ class Data:
         return self.location
 
 
+# def make_people_table(firstname, lastname, status, job_title, salary, hours, agency):
+ #   os.system(f'python3 people.py {firstname} {lastname} {status} {job_title} {salary} {hours} {agency}')
 
-
-#def make_people_table(firstname, lastname, status, job_title, salary, hours, agency):
- #   os.system(f'python3 people.py {firstname} {lastname} {status} {job_title} {salary} {hours} {agency}')    
-    
 def make_agency_table(agency_name, agency_borough):
     os.system(f'python3 agency.py {agency_name} {agency_borough}')
 
+
 def make_housing():
     os.system(f'python3 housing.py')
-        
+
+
 def main():
     os.system('sqlite3 nyc.db < schema.sql')
     data = Data("Citywide_Payroll_Data__Fiscal_Year_.csv")
@@ -83,10 +84,9 @@ def main():
     lst = []
     for i in range(1000):
         #make_people_table(lastname_df[i], firstname_df[i], status_df[i], title_df[i], salary_df[i], hours_df[i], agency_df[i])
-        make_agency_table(agency_df[i], location_df[i] )
+        make_agency_table(agency_df[i], location_df[i])
     make_housing()
 
 
 if __name__ == '__main__':
     main()
-
