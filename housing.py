58234b75c31733d1715655897795478e012d9185
Alyssa <alyssanm18@gmail.com>
2022-05-01 22:48:29 -0600

    fixed typo

--- housing.py
+++ housing.py
@@ -5,11 +5,16 @@ import sys
 con = sqlite3.connect('nyc.db')
 cur = con.cursor()
 
-cur.execute("INSERT INTO HOUSING(borough, avg_rent, avg_buy, bedrooms) VALUES('MANHATAN', 3785, 1290000, 1)")
-cur.execute("INSERT INTO HOUSING(borough, avg_rent, avg_buy, bedrooms) VALUES('BROOKLYN', 3350, 969000, 1)")
-cur.execute("INSERT INTO HOUSING(borough, avg_rent, avg_buy, bedrooms) VALUES('QUEENS', 2242, 699000, 1)")
-cur.execute("INSERT INTO HOUSING(borough, avg_rent, avg_buy, bedrooms) VALUES('STATEN ISLAND', 1500, 579908, 1)")
-cur.execute("INSERT INTO HOUSING(borough, avg_rent, avg_buy, bedrooms) VALUES('BRONX', 1800, 550000, 1)")
+cur.execute(
+    "INSERT INTO HOUSING(borough, avg_rent, avg_buy, bedrooms) VALUES('MANHATTAN', 3785, 1290000, 1)")
+cur.execute(
+    "INSERT INTO HOUSING(borough, avg_rent, avg_buy, bedrooms) VALUES('BROOKLYN', 3350, 969000, 1)")
+cur.execute(
+    "INSERT INTO HOUSING(borough, avg_rent, avg_buy, bedrooms) VALUES('QUEENS', 2242, 699000, 1)")
+cur.execute(
+    "INSERT INTO HOUSING(borough, avg_rent, avg_buy, bedrooms) VALUES('STATEN ISLAND', 1500, 579908, 1)")
+cur.execute(
+    "INSERT INTO HOUSING(borough, avg_rent, avg_buy, bedrooms) VALUES('BRONX', 1800, 550000, 1)")
 
 con.commit()
 cur.close()
