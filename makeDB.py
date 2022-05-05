7c3393fa2732d620df7f2136c4247e67658244e7
Kalicia <d00372177@dmail.dixie.edu>
2022-05-04 22:15:52 -0600

    updated makeDB

--- makeDB.py
+++ makeDB.py
@@ -91,7 +91,6 @@ def main():
     base_salary_df = data.getBaseSalary()
     lst = []
 
-<<<<<<< HEAD
     
     for i in range(100):
         Lastname = '"'+lastname_df[i]+'"'
@@ -118,13 +117,6 @@ def main():
         make_people_table(Firstname, Lastname, Status, title, salary, hours, agency, location)
         #make_jobs(title_df[i], agency_df[i], base_salary_df[i])
     make_housing()
-=======
-    make_housing()
-    for i in range(100):
-        #make_people_table(lastname_df[i], firstname_df[i], status_df[i],title_df[i], salary_df[i], hours_df[i], agency_df[i])
-        make_agency_table(agency_df[i], location_df[i])
-        make_jobs(title_df[i], agency_df[i], base_salary_df[i], location_df[i])
->>>>>>> 2e7b77186ad06a4c1920fdb7517414e2c7949958
 
 
 if __name__ == '__main__':
